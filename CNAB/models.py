c5b2a01ad92c6e83bad235b54374579a83db95a0
matheusprado1 <matheusprado1096@gmail.com>
2022-10-14 20:12:25 -0300

    create: models

--- CNAB/models.py
+++ CNAB/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
+from django.core.validators import MinValueValidator, MaxValueValidator, MinLengthValidator, MaxLengthValidator
 
-# Create your models here.
+
+class CNAB(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    type = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(9)])
+    date = models.TextField(max_length=8, validators=[
+                            MinLengthValidator(8), MaxLengthValidator(8)])
+    value = models.TextField(max_length=10)
+    cpf = models.TextField(max_length=11, validators=[
+                           MinLengthValidator(11), MaxLengthValidator(11)])
+    card = models.TextField(max_length=12, validators=[
+                            MinLengthValidator(12), MaxLengthValidator(12)])
+    hour = models.TextField(max_length=6, validators=[
+                            MinLengthValidator(6), MaxLengthValidator(6)])
+    owner = models.TextField(max_length=14)
+    name = models.TextField(max_length=19)
